a26b111169c88cb87a02c2e7a0a6704ddaf58fa6
IgorKhvostik <khvostykihor@gmail.com>
2017-10-31 13:19:57 +0200

    db seeds

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0;$i<10;$i++){
+            $name_list=array('retard','kek','Tolya','mannequin');
+            $sec_name_list=array('ant', 'jobs', 'keks');
+            $key_first=array_rand($name_list);
+            $key_sec=array_rand($sec_name_list);
+            DB::table('users')->insert([
+                'first_name' =>$name_list[$key_first],
+                'second_name' =>$sec_name_list[$key_sec],
+                'password' => bcrypt('secret')]);
+        }
+    }
+}
